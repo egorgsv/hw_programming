2c76d1705d97fb1afbd3da98b3c9ba3f9a846c88
egorgsv <egorspb3@gmail.com>
2021-03-11 19:22:32 +0300

    аргументы командной строки

--- HW2/CsharpProj/Tests/FloydWarshallTest.cs
+++ HW2/CsharpProj/Tests/FloydWarshallTest.cs
@@ -1,4 +1,3 @@
-
 using System;
 using CsharpProj;
 using Matrix;
--- HW2/CsharpProj/TransitiveClosure/Program.cs
+++ HW2/CsharpProj/TransitiveClosure/Program.cs
@@ -10,10 +10,9 @@ namespace TransitiveClosure
     {
         static void Main(string[] args)
         {
-            String path = "/Users/egorgusev/Programming/hw_programming/HW2/CsharpProj/Tests/test_data/bool.txt";
-            //String path = Console.ReadLine(),
-                //output = Console.ReadLine();
-            Boolean[][] array = MatrixReader<Boolean>.Reader(path);
+            String path = args[0],
+                output = args[1];
+            Boolean[][] array = MatrixIO<Boolean>.Reader(path);
 
             try
             {
@@ -25,15 +24,11 @@ namespace TransitiveClosure
                     StringBuilder builder = new StringBuilder();
                     builder.AppendLine("digraph G {");
                     for (int i = 0; i < transClosureMatrix.Length; ++i)
-                        builder.Append(i.ToString());
-                    builder.AppendLine(";");
-                    for (int i = 0; i < transClosureMatrix.Length; ++i)
                         for (int j = 0; j < transClosureMatrix[0].Length; ++j)
                             if (transClosureMatrix[i][j].value)
                                 builder.Append(i.ToString() + " -> " + j.ToString() + " [" + (matrix[i][j].value ? "" : "color=red") + "];");
-                    builder.AppendLine(";");
+                    builder.AppendLine("}");
                     return builder.ToString();
-
                 }
 
                 void GeneratePDF(String res, String dot)
@@ -54,6 +49,7 @@ namespace TransitiveClosure
 
                 string outputDot = GetDotCode(mtrx.array, transClosureMtrx.array);
                 GeneratePDF("graphVis.pdf", outputDot);
+                MatrixIO<Boolean>.WriteMatrix(transClosureMtrx.array, output);
             }
             catch (ArgumentException exception)
             {
@@ -62,3 +58,4 @@ namespace TransitiveClosure
         }
     }
 }
+
