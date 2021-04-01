94891f5a9d1dc5bd4011fee1715b22889f925b1c
egorgsv <egorspb3@gmail.com>
2021-04-01 12:07:50 +0300

    added new Floyd-Warshall Tests

--- HW2/CsharpTasks/Tests/FloydWarshallTest.cs
+++ HW2/CsharpTasks/Tests/FloydWarshallTest.cs
@@ -38,6 +38,36 @@ namespace Tests
         }
 
         [Test]
+        public void InfinityAllPairsShortestPathTest()
+        {
+            string[][] table1 =
+                {
+                    new[] {"0.0", "inf", "inf", "inf"},
+                    new[] {"inf", "0.0", "inf", "inf"},
+                    new[] {"inf", "inf", "0.0", "inf"},
+                    new[] {"inf", "inf", "inf", "0.0"}
+                },
+                table2 =
+                {
+                    new[] {"0.0", "inf", "inf", "inf"},
+                    new[] {"inf", "0.0", "inf", "inf"},
+                    new[] {"inf", "inf", "0.0", "inf"},
+                    new[] {"inf", "inf", "inf", "0.0"}
+                };
+
+            var origin = ToExtendedRealMatrix(table1);
+            var expected = ToExtendedRealMatrix(table2);
+
+            var result = FloydWarshall<ExtendedReal>.Execute(origin, new ExtendedRealSemigroup());
+
+            for (var i = 0; i < expected.Columns; ++i)
+                for (var j = 0; j < expected.Columns; ++j)
+                    Assert.AreEqual(expected.Array[i][j].ToWord(), result.Array[i][j].ToWord());
+        }
+        
+        
+        
+        [Test]
         public void AllPairsShortestPathTest()
         {
             string[][] table1 =
@@ -53,6 +83,28 @@ namespace Tests
                     new[] {"4.0", "0.0", "2.0", "4.0"},
                     new[] {"5.0", "1.0", "0.0", "2.0"},
                     new[] {"3.0", "-1.0", "1.0", "0.0"}
+                };
+
+            var origin = ToExtendedRealMatrix(table1);
+            var expected = ToExtendedRealMatrix(table2);
+
+            var result = FloydWarshall<ExtendedReal>.Execute(origin, new ExtendedRealSemigroup());
+
+            for (var i = 0; i < expected.Columns; ++i)
+                for (var j = 0; j < expected.Columns; ++j)
+                    Assert.AreEqual(expected.Array[i][j].ToWord(), result.Array[i][j].ToWord());
+        }
+        
+        [Test]
+        public void OneNodeAllPairsShortestPathTest()
+        {
+            string[][] table1 =
+                {
+                    new[] {"0.0"}
+                },
+                table2 =
+                {
+                    new[] {"0.0"}
                 };
 
             var origin = ToExtendedRealMatrix(table1);
@@ -82,6 +134,59 @@ namespace Tests
                 new[] {true, true, true, false},
                 new[] {true, true, true, false},
                 new[] {true, true, true, true}
+            };
+
+            var origin = ToBoolMatrix(array0);
+            var expected = ToBoolMatrix(array1);
+
+            var result = FloydWarshall<Boolean>.Execute(origin, new BooleanSemigroup());
+
+            for (var i = 0; i < expected.Columns; i++)
+                for (var j = 0; j < expected.Columns; j++)
+                    Assert.AreEqual(expected.Array[i][j].Value, result.Array[i][j].Value);
+        }
+        
+        [Test]
+        public void FalseTransitiveClosureTest()
+        {
+            bool[][] array0 =
+            {
+                new[] {true, false, false, false},
+                new[] {false, true, false, false},
+                new[] {false, false, true, false},
+                new[] {false, false, false, true}
+            };
+
+            bool[][] array1 =
+            {
+                new[] {true, false, false, false},
+                new[] {false, true, false, false},
+                new[] {false, false, true, false},
+                new[] {false, false, false, true}
+            };
+
+            var origin = ToBoolMatrix(array0);
+            var expected = ToBoolMatrix(array1);
+
+            var result = FloydWarshall<Boolean>.Execute(origin, new BooleanSemigroup());
+
+            for (var i = 0; i < expected.Columns; i++)
+                for (var j = 0; j < expected.Columns; j++)
+                    Assert.AreEqual(expected.Array[i][j].Value, result.Array[i][j].Value);
+        }
+        
+        
+        [Test]
+        public void OneNodeTransitiveClosureTest()
+        {
+            bool[][] array0 =
+            {
+                new[] {true}
+            };
+
+            bool[][] array1 =
+            {
+                new[] {true}
             };
 
             var origin = ToBoolMatrix(array0);
